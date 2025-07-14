bd9e1624b1e13a5ddba0fd18ef556acdd82622f5
a920604a <a920604a@gmail.com>
2025-07-14 10:15:16 +0800

    update celery

--- backend/celery_worker.py
+++ backend/celery_worker.py
@@ -1,10 +1,12 @@
 # backend/celery_worker.py
 from celery import Celery
 import os
+
 redis_host = os.getenv("REDIS_HOST", "localhost")
 
 celery_app = Celery(
     "tasks",
     broker=f"redis://{redis_host}:6379/0",  # Redis 為任務佇列中介
     backend=f"redis://{redis_host}:6379/1",  # 追蹤任務狀態用
+    include=["tasks.train_model_task"],
 )
--- backend/tasks/train_model_task.py
+++ backend/tasks/train_model_task.py
@@ -9,6 +9,9 @@ from src.train_config import TrainConfig
 def train_model_task(self, ticker: str, exchange: str, config_dict: dict):
     try:
         config = TrainConfig(**config_dict)
+        print(f"ticker {ticker}")
+        print(f"exchange {exchange}")
+        print(f"config {config}")
         rmse, run_id = train_and_register(ticker, exchange, config)
         return {"status": "completed", "ticker": ticker, "rmse": rmse, "run_id": run_id}
     except Exception as e:
