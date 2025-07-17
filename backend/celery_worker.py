3cd9931aca701ffd8764059161a237119123c3e9
a920604a <a920604a@gmail.com>
2025-07-17 09:50:17 +0800

    add celery exporter

--- backend/celery_worker.py
+++ backend/celery_worker.py
@@ -1,6 +1,7 @@
 # celery_worker.py
 from celery import Celery
 import os
+from celery_prometheus import add_prometheus_option  # 新增導入
 
 redis_host = os.getenv("REDIS_HOST", "localhost")
 
@@ -10,3 +11,12 @@ celery_app = Celery(
     backend=f"redis://{redis_host}:6379/1",  # 追蹤任務狀態用
     include=["tasks.train_model_task"],
 )
+
+# 確保 Celery Worker 發送事件
+celery_app.conf.update(
+    worker_send_task_events=True,
+    task_send_sent_event=True,
+)
+
+# 添加 Prometheus 選項
+add_prometheus_option(celery_app)
